c4487bbcd754fc1aec98bf641819bdcf6b859746
emrebbx <emreugurer94@gmail.com>
2026-08-20 21:06:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 const acilisEkrani = document.getElementById("acilisEkrani");
 const anaMenu = document.getElementById("anaMenu");
+const oynanisEkrani = document.getElementById("oynanisEkrani");
 
 const devamButonu = document.getElementById("devamButonu");
 const baslaButonu = document.getElementById("baslaButonu");
@@ -14,10 +15,7 @@ const nasilOynanirButonu = document.getElementById("nasilOynanirButonu");
    SES SEVİYELERİ
 ========================= */
 
-/* Müziklerin ulaşacağı maksimum ses */
 const MUZIK_SESI = 0.22;
-
-/* Buton sesi */
 const BUTON_SESI = 0.65;
 
 
@@ -31,7 +29,6 @@ const oyunMuzik = new Audio("sounds/oyun-muzik.mp3");
 anaMenuMuzik.loop = true;
 oyunMuzik.loop = true;
 
-/* Başlangıçta sessiz */
 anaMenuMuzik.volume = 0;
 oyunMuzik.volume = 0;
 
@@ -129,18 +126,15 @@ devamButonu.addEventListener("click", () => {
 
   butonSesiCal();
 
-  /* Ana menüye geç */
   acilisEkrani.classList.remove("aktif");
   anaMenu.classList.add("aktif");
 
-  /* Menü müziğini sessiz başlat */
   anaMenuMuzik.currentTime = 0;
   anaMenuMuzik.volume = 0;
 
   anaMenuMuzik.play()
     .then(() => {
 
-      /* Yavaşça aç */
       muzikAc(
         anaMenuMuzik,
         MUZIK_SESI,
@@ -170,9 +164,7 @@ baslaButonu.addEventListener("click", async () => {
 
   /*
     Oyun müziğini kullanıcı tıklaması sırasında
-    sessiz şekilde başlatıyoruz.
-    Böylece mobil tarayıcıların ses engeline
-    takılmıyoruz.
+    sessiz başlatıyoruz.
   */
 
   oyunMuzik.currentTime = 0;
@@ -188,7 +180,10 @@ baslaButonu.addEventListener("click", async () => {
   });
 
 
-  /* Menü müziğini yavaşça kapat */
+  /*
+    Ana menü müziğini yavaşça azalt.
+  */
+
   await muzikKapat(
     anaMenuMuzik,
     1000
@@ -196,14 +191,18 @@ baslaButonu.addEventListener("click", async () => {
 
 
   /*
-    BURADA DAHA SONRA
-    OYUN EKRANINA GEÇİŞ YAPACAĞIZ
+    ANA MENÜ → OYNANIŞ EKRANI
   */
 
-  console.log("Oyun ekranına geçiliyor!");
+  anaMenu.classList.remove("aktif");
+  oynanisEkrani.classList.add("aktif");
 
 
-  /* Oyun müziğini yavaşça aç */
+  /*
+    Oyun ekranı açıldıktan sonra
+    oyun müziğini yavaşça yükselt.
+  */
+
   muzikAc(
     oyunMuzik,
     MUZIK_SESI,
