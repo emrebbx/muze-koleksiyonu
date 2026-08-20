f450ee91d4f708f1bf2fff7e1700ad2cd7252838
emrebbx <emreugurer94@gmail.com>
2026-08-20 18:59:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const acilisEkrani = document.getElementById("acilisEkrani");
+const anaMenu = document.getElementById("anaMenu");
 const devamButonu = document.getElementById("devamButonu");
 
 devamButonu.addEventListener("click", () => {
-  console.log("Devam butonuna basıldı!");
+
+  acilisEkrani.classList.remove("aktif");
+  anaMenu.classList.add("aktif");
+
 });
